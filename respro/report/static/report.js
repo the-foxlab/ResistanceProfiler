47a29fde41dbab78ca9402ec550a3bfdf0bf8bde
jonas-fuchs <jonas.michael.fuchs@googlemail.com>
2026-05-26 12:27:13 +0200

    added tabular download button

--- respro/report/static/report.js
+++ respro/report/static/report.js
@@ -459,7 +459,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const blobUrl = URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = blobUrl;
-        link.download = 'mutations.filtered.tsv';
+        link.download = 'variant_profile.tsv';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
